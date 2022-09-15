4c91ddc1bb13fbe67a8572f747bb8ecfbf46f6b6
subbaramanak <akshya.sri@hotmail.com>
2022-09-14 21:37:41 -0700

    cleaned up helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,12 @@
-
+/* Name: Akshya Subbaraman
+* Purpose: Write the Hellow World Program with 5 lines of output
+* Pseudocode: Program is stored in class helloworld. Use for loop to print out "hello world" 5 times.
+* Maintenance Log: 9/14/22- Finished Hello World program
+*/
 public class helloworld {
     public static void main(String[] args) {
-        for(int i =1; i<6; i++)
-
-        {
+        for(int i =1; i<6; i++) {
             System.out.println("hello world");
         }
     }
-
 }
