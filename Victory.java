349d3616608260800cbbb6c9bd231a069e30da88
subbaramanak <akshya.sri@hotmail.com>
2022-09-14 21:26:49 -0700

    cleaned up victory.java

--- Victory.java
+++ Victory.java
@@ -1,19 +1,17 @@
 /* Name: Akshya Subbaraman
-* Purpose: Chapter 1 Exercise 7- Write a program the prints the "Victory is mine!" output.
-* Psuedocode: Use a for loop to print out text from drawvictory method.
-* Maintanence Log: 9/14/22- Finished Program */
+* Purpose: Chapter 1 Exercise 7 - Write a program the prints the "Victory is mine!" output.
+* Pseudocode: Use a for loop to print out text from drawVictory method.
+* Maintenance Log: 9/14/22- Finished Program
+*/
 public class Victory {
     public static void main(String[] args) {
         System.out.println("//////////////////////");
-        for (int i=0; i<5; i++) {
-            drawvictory();
+        for (int i = 0; i < 5; i++) {
+            drawVictory();
         }
-
     }
 
-    public static void drawvictory()
-    {
-
+    public static void drawVictory() {
         System.out.println("|| Victory is mine! ||");
         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
 
