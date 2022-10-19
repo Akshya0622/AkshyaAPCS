0dbb5399c052c6a8ec3173ad21325534bef1405a
subbaramanak <akshya.sri@hotmail.com>
2022-10-18 19:57:16 -0700

    finished stairstring.java

--- stairstring.java
+++ stairstring.java
@@ -3,11 +3,21 @@
 The method will divide the String into the number of substrings indicated
 and then output the substrings so that each substring is placed one line
 below the next in the same horizontal location it would have been had they been on the same line.
+* Pseudocode: Take String and int input from user and send to method stairString.
+perLine calculates how many characters should be in each row and leftOver counts how many
+characters will remain that would need to be added onto the last row. A number of spaces equal to the
+length of the characters already printed in the previous line are printed.
+A substring prints from the string at the index i, i + perLine.
+Where i is incremented by the number of characters there should be perLine.
+The substring prints until i reaches a value that is the length of the string - the left overs.
+The left overs are added to the end of the last row.
+* Maintenance Log: 10/17/22 - Misunderstood problem and made a program that prints each word in a sentence on different lines.
+10/18/22 - Finished Program.
 * */
 import java.util.*;
 public class stairstring {
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         System.out.print("String: ");
         Scanner input = new Scanner(System.in);
@@ -21,31 +31,27 @@ public class stairstring {
 
     public static void stairString(String words, int numLines) {
 
-        Scanner input = new Scanner(words);
+        if (numLines > 0) {
 
-        int count = 0;
-        String last = "";
-        while (input.hasNext()) {
-            last = input.next();
-            count++;
+            Scanner input = new Scanner(words);
+            int perLine = words.length() / numLines;
+            int leftOver = words.length() % numLines;
+            int newVar = 0;
+
+            for (int i = 0; i < words.length() - leftOver; i += perLine) {
+                System.out.println();
+                for (int spaces = 0; spaces < i; spaces++) {
+                    System.out.print(" ");
+                }
+                System.out.print(words.substring(i, i + perLine));
+                newVar = i;
+            }
+
+            System.out.print(words.substring(newVar + perLine, words.length()));
+
         }
 
-        String everythingButLast = "";
-        Scanner input2 = new Scanner(words);
-        for (int i = 1; i < count; i++) {
-            everythingButLast += input2.next();
-            everythingButLast += " ";
-        }
 
-        String spaces = "";
-        for(int i = 0; i <= everythingButLast.length(); i++) {
-            spaces += " ";
-        }
 
-        System.out.print(words.substring(0,everythingButLast.length()));
-        for (int i = 0; i < numLines; i++) {
-            System.out.println();
-        }
-        System.out.print(spaces + last);
     }
 }
